ee44511fc9f521a6a066781b40bc3479db200cfb
Agustin Sandoval <94272906+Agustincomics@users.noreply.github.com>
2023-04-26 18:39:16 -0300

    nueva tarea

---
+++ tp2_1_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // codigo a completar
+    #define N 20
+    int i;
+    double vt[N];
+    for(i = 0; i < N; i++)
+    {
+        vt[i]=1+rand()%100;
+        printf("%f\n", vt[i]);
+    }
+
+
+
+    return 0;
+}
